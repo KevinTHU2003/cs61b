accf971cacd6bdc3636fd43746ce1c049fd73959
KevinTHU2003 <zkw21@mails.tsinghua.edu.cn>
2022-02-15 16:49:21 +0800

    Lab2 correction: fix null exception

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,10 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if(A == null){
+            A = B;
+            return A;
+        }
         IntList p = A;
         while (p.rest != null){
             p = p.rest;
@@ -94,6 +98,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if(A == null){
+            return B;
+        }
         if(A.rest == null) {
             return new IntList(A.first, B);
         }
